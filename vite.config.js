78318b2aabca422c4d65a5314f05e0a9057edfd5
FERNANDO CUNHA <101639298+omega3344@users.noreply.github.com>
2023-12-07 23:30:51 +0000

    commit 5

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       'process.env.VITE_YOUR_TEMPLATE_ID': JSON.stringify(env.VITE_YOUR_TEMPLATE_ID),
       'process.env.VITE_YOUR_PUBLIC_KEY': JSON.stringify(env.VITE_YOUR_PUBLIC_KEY)
     },
-    base: "/gil",
+    base: "/",
     plugins: [react()],
   }
 })
